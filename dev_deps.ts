491ab0df8bc59af297aa73b59342dd9479894c14
realpha <0xrealpha@proton.me>
2023-11-13 16:23:17 +0100

    feat(scripts): first take on npm build script

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,2 +1,4 @@
 export * from "https://deno.land/std@0.196.0/assert/mod.ts";
 export * from "https://deno.land/std@0.196.0/testing/types.ts";
+export * as dnt from "https://deno.land/x/dnt@0.38.1/mod.ts";
+export * as semver from "https://deno.land/std@0.206.0/semver/mod.ts";
